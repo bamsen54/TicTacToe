a0798f1d264fd5bc4a28dab3a2e7f24ecc19f672
bamsen54 <149212781+bamsen54@users.noreply.github.com>
2025-09-24 11:50:14 +0200

    First Commit

---
+++ TicTacToe/src/Game.java
@@ -0,0 +1,11 @@
+
+// keeps track of all information related to a tictactoe game
+
+public class Game {
+
+    final char[][] board = { {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}, };
+
+    char turn = 'b';
+}
